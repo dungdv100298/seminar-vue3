3ef4e3a1cb12815cb9e32c808018ff1f2757f480
dungdv2 <dungdv2@vmodev.com>
2023-06-19 01:32:43 +0700

    firt commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>Hello VMO</p>
+  <p>Hello Infility Team</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+
   }
 }
 </script>
